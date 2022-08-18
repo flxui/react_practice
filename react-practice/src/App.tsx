3f05cc2a0ebd534b2c3e1964442ff5f09beb21ab
flxui <felix.burdot@gmail.com>
2022-08-18 20:37:14 +0200

    Add button to remove filters

--- react-practice/src/App.tsx
+++ react-practice/src/App.tsx
@@ -9,48 +9,55 @@ import List from './List'
 
 function App() {
 
-  let [searchParams, setSearchParams] = useSearchParams()
-  const obj = Object.fromEntries([...searchParams])
-
-  useEffect(() => {
+    let [searchParams, setSearchParams] = useSearchParams()
     const obj = Object.fromEntries([...searchParams])
-    obj.page = '1'
-    setSearchParams(obj)
-  }, [])
+
+    useEffect(() => {
+        const obj = Object.fromEntries([...searchParams])
+        obj.page = '1'
+        setSearchParams(obj)
+    }, [])
 
 
-  function filterOutNumbers() {
-    obj.filterOutNumbers = 'true'
-    setSearchParams(obj)
-  }
+    function filterOutNumbers() {
+        obj.filterOutNumbers = 'true'
+        setSearchParams(obj)
+    }
 
-  function sortAlphabetically() {
-    obj.sortAlphabetically = 'true'
-    setSearchParams(obj)
-  }
+    function sortAlphabetically() {
+        obj.sortAlphabetically = 'true'
+        setSearchParams(obj)
+    }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <List></List>
-        <Button text="Filter Out Numbers" onClickFunction={() => filterOutNumbers()} ></Button>
-        <Button text="Sort Alphabetically" onClickFunction={() => sortAlphabetically()} ></Button>
-        <Searchbar></Searchbar>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    function removeFilters() {
+        delete obj.sortAlphabetically
+        delete obj.filterOutNumbers
+        setSearchParams(obj)
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <List></List>
+                <Button text="Filter Out Numbers" onClickFunction={() => filterOutNumbers()} ></Button>
+                <Button text="Sort Alphabetically" onClickFunction={() => sortAlphabetically()} ></Button>
+                <Button text="Remove Filters" onClickFunction={() => removeFilters()} ></Button>
+                <Searchbar></Searchbar>
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
